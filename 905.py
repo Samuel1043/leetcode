3a458bb0fa26cef284e2f3ec8ee8f28d86cad27e
samuel1043 <babylily1811@gmail.com>
2021-02-20 15:15:05 +0800

    update 905.py

---
+++ 905.py
@@ -0,0 +1,14 @@
+def sortArrayByParity(self, A: List[int]) -> List[int]:
+    N=len(A)
+    i=0
+    j=N-1
+    while i<j:
+        cur=A[i]
+        if cur%2==0:
+            i+=1
+        else:
+            tmp=A[i]
+            A[i]=A[j]
+            A[j]=tmp
+            j-=1
+    return A
